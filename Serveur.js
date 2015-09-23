23b8468554e6e3263577ba336feb220ef0aa6b23
tobiasruenzi <tobias.ruenzi@bluewin.ch>
2015-09-23 16:51:45 +0200

    2eme commit
    premier essai, balle bouge, et change de direction, essai avec tableau
    de cercles

--- Serveur.js
+++ Serveur.js
@@ -1,23 +1,14 @@
-var fs = require('fs');
-var express = require('express');
+
 var https = require('https');
-var key = fs.readFileSync('private/key.pem');
-var cert = fs.readFileSync('private/cert.pem')
-var https_options = {
-    key: key,
-    cert: cert
+var fs = require('fs');
+var options = {
+    key: fs.readFileSync('private/key.pem'),
+    cert: fs.readFileSync('private/cert.pem')
 };
-var PORT = 8000;
-var HOST = 'localhost';
-app = express();
-
-app.configure(function(){
-    app.use(app.router);
-});
-
-server = https.createServer(https_options, app).listen(PORT, HOST);
-console.log('HTTPS Server listening on %s:%s', HOST, PORT);
-
-app.get('/', function(req, res) {
-    res.sendfile('public/index.html');
-});
+// Chargement du fichier html affiché au client
+var server = https.createServer(options,function(req, res) {
+    fs.readFile('./public/Index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+}).listen(8080);
