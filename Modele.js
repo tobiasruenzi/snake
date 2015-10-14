5566ba018b2fe7527b685147e25c4c5bf51fcf32
tobiasruenzi <tobias.ruenzi@bluewin.ch>
2015-10-14 17:21:38 +0200

    creation de jeu
    creation de jeu avec deux joueurs, attribution d'un petit serpent de
    trois points

---
+++ Modele.js
@@ -0,0 +1,24 @@
+module.exports.Serpent=Serpent;
+module.exports.Point=Point;
+module.exports.Jeu=Jeu;
+module.exports.Joueur=Joueur;
+
+function Point(Coordx, Coordy) {
+	this.x=Coordx;
+	this.y=Coordy;
+	
+};
+function Serpent(Point1, Point2, Point3){
+	this.Tableau = [Point1, Point2, Point3];
+}
+function Joueur(Serpent, ws) {
+	this.Serpent=Serpent;
+	this.ws=ws
+};
+function Jeu (Joueur1, Joueur2) {
+	this.Joueur1=Joueur1;
+	this.Joueur2=Joueur2;
+	this.update= function(){
+		
+	}
+}
--- Serveur.js
+++ Serveur.js
@@ -1,32 +1,60 @@
 
 var https = require('https');
 var fs = require('fs');
+var Model = require('./Modele');
 
 var options = {
-    key: fs.readFileSync('private/key.pem'),
-    cert: fs.readFileSync('private/cert.pem')
+		key: fs.readFileSync('private/key.pem'),
+		cert: fs.readFileSync('private/cert.pem')
 };
-// Chargement du fichier html affiché au client
+//Chargement du fichier html affiché au client
 var server = https.createServer(options,function(req, res) {
-    fs.readFile('./public/Index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
+	fs.readFile('./public/Index.html', 'utf-8', function(error, content) {
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.end(content);
+	});
 }).listen(8666);
 
 var WebSocketServer = require('ws').Server
-, wss = new WebSocketServer({port: 8667});
+, wss = new WebSocketServer({ port:8667});
+var Joueurs = new Array(2);
+
+if(Joueurs.length!=(Joueurs[0]+1)){
 wss.on('connection', function(ws) {
-  ws.on('message', function(message) {
-	  var point = JSON.parse(message);
-      console.log('received: %s', point);
-      var x0= point.X[0];
-      var y0= point.Y[0];
-      console.log('point1 %s,%s', x0, y0);
-      x0++;
-     
-      console.log('point1 modifié %s,%s', x0, y0);
-  });
-  ws.send('Hello');
+	
+	
+		
+		var posx= Math.random()*430+200;
+		var posy= Math.random()*260+200;
+		var Point1= new Model.Point(posx, posy);
+		var Point2 =new Model.Point(posx, posy+50);
+		var Point3 = new Model.Point(posx, posy+100);
+		var Serpent= new Model.Serpent(Point1, Point2, Point3);
+		var Joueur= new Model.Joueur(Serpent, ws);
+		Joueurs.push(Joueur);
+		console.log('attendu %s %s', posx,posy);
+		console.log(' Joueur x et y %s %s',Joueur.Serpent.Tableau[0].x, Joueur.Serpent.Tableau[0].y);
+	
+
+
+	
 });
+}
+
+
+/*
+		  var point = JSON.parse(message);
+	      console.log('received: %s', point);
+	      var x0= point.X[0];
+	      var y0= point.Y[0];
+	      console.log('point1 %s,%s', x0, y0);
+	      x0++;
+
+	      console.log('point1 modifié %s,%s', x0, y0);
+	  });
+ */
+
+
+var Jeu= Model.Jeu(Joueurs[0],Joueurs[1]);
+
 
