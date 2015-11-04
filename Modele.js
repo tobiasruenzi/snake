93674367e0d0bd255d131ff5c88bce7c551d1e27
tobiasruenzi <tobias.ruenzi@bluewin.ch>
2015-11-04 08:33:56 +0100

    nombre de joueurs non fixe
    création dynamiques des joueurs et des serpent, bcp de changement au
    code, remplacement du code en double par des boucles for

--- Modele.js
+++ Modele.js
@@ -1,16 +1,18 @@
-module.exports.Serpent=Serpent;
 module.exports.Point=Point;
 module.exports.Joueur=Joueur;
 
 function Point(Coordx, Coordy) {
 	this.x=Coordx;
 	this.y=Coordy;
-	
+
 };
-function Serpent(Point1){
-	this.Tableau = [Point1];
-}
-function Joueur(Serpent, ws) {
-	this.Serpent=Serpent;
-	this.ws=ws
+
+function Joueur(Point, ws, nexttete) {
+	this.Serpent=[Point];
+
+	this.ws=ws;
+	this.nexttete=nexttete;
+	this.grandir=function(point){
+		this.Serpent.push(point);
+	};
 };
--- Serveur.js
+++ Serveur.js
@@ -17,7 +17,7 @@ app.configure(function(){
 	app.use(app.router);
 });
 
-var server = https.createServer(options, app).listen(PORT, HOST);
+var server = https.createServer(options, app).listen(PORT);
 console.log('HTTPS Server listening on https://%s:%s', HOST, PORT);
 
 app.get('/', function(req, res) {
@@ -32,151 +32,136 @@ app.configure(function() {
 var WebSocketServer = require('ws').Server
 , wss = new WebSocketServer({ server:server});
 var Joueurs = [];
-var nexttete1= [0, 5];
-var nexttete2= [0, 5];
+var nexttetebase= [0,5];
 var taille=50;
-var ws1;
-var ws2;
-wss.on('connection', function(ws) {
-	if(Joueurs.length===0){
 
-		ws1=ws;
-		var posx1= 550;
-		var posy1= 600;
-		var Point1= new Model.Point(posx1, posy1);
-		var Serpent1= new Model.Serpent(Point1);
-		for(var t=1; t<taille;t++){
-			var Point11= new Model.Point(posx1, posy1-t*2);
-			Serpent1.Tableau.push(Point11);
-		}
-		var Joueur1= new Model.Joueur(Serpent1, ws);
+setInterval(onFrame, 10);
 
-		Joueurs.push(Joueur1);
-		console.log('premier joueur connecté, attente du second');
-		
-		
+wss.on('connection', function(ws) {	
+	console.log("Nouveau Joueur")
+	var posx= Math.floor((Math.random() * 1150) + 50);
+	var posy= Math.floor((Math.random() * 600) + 50);
+	var Point= new Model.Point(posx, posy);
+
+	var Joueur =new Model.Joueur(Point, ws, nexttetebase);
+	for(var t=1; t<taille;t++){
+		Joueur.grandir(new Model.Point(posx, posy-t*2));
 	}
-	else if(Joueurs.length===1){
 
-		ws2=ws;
-		var posx2= 950;
-		var posy2= 600;
-		var Point2= new Model.Point(posx2, posy2);
-		var Serpent2= new Model.Serpent(Point2);
-		for(var t1=1; t1<taille;t1++){
-			var Point22= new Model.Point(posx2, posy2-t1*2);
-			Serpent2.Tableau.push(Point22);
-		}
-		var Joueur2= new Model.Joueur(Serpent2, ws);
 
-		Joueurs.push(Joueur2);
-		console.log('second joueur connecté, lancement');
-		
-		
-		begin(Joueurs);
-	}
-	else{
-		console.log('deja deux joueurs');
-		ws.close();
-	}
-	
+	Joueurs.push(Joueur);
+
+
+
+	begin(Joueurs);
+
+
+
+
 	ws.on('message', function message(event) 
 	{
 		var msg = JSON.parse(event);
-		
+
 		switch(msg.type)
 		{				
 			case "tete" :
-				console.log("maj tete recu %s %s", msg.x, msg.y);
-				if (ws===ws1)
-					{
-						nexttete1=[msg.x,msg.y];
+				for(var q=0;q<Joueurs.length;q++){
+					if(ws===Joueurs[q].ws){
+						Joueurs[q].nexttete=[msg.x,msg.y];
 					}
-				else if (ws===ws2)
-					{
-						nexttete2=[msg.x,msg.y];
-					}
-				
+				}
 				break;
 		}		
-		
-		
-		
+
+
+
 	});
-	
-	
-	
-	
+
+
 });
 
-
+function onFrame() 
+{
+	update();
+}
 function begin(Joueurs){
-	if(Joueurs.length===2){
-		
-		console.log('serpent1 %s %s', Joueurs[0].Serpent.Tableau[0].x, Joueurs[0].Serpent.Tableau[0].y);
-		var message1 = { 
-				type : "creeserpents",
-				Serpent1 : Joueurs[0].Serpent.Tableau,
-				Serpent2 : Joueurs[1].Serpent.Tableau
+	var TabSerpent=[];
+	for(var i=0;i<Joueurs.length;i++){
+		TabSerpent.push(Joueurs[i].Serpent);
+	}
+	for(var y=0;y<Joueurs.length;y++){
+		var message = { 
+				type : "majserpents",
+				Serpent : TabSerpent,
+				index : y
 		};
-		ws1.send(JSON.stringify(message1));
-		console.log('serpent2 %s %s', Joueurs[1].Serpent.Tableau[0].x, Joueurs[1].Serpent.Tableau[0].y);
-		var message2 = { 
-				type : "creeserpents",
-				Serpent1 : Joueurs[1].Serpent.Tableau,
-				Serpent2 : Joueurs[0].Serpent.Tableau
+		try { 
+			Joueurs[y].ws.send(JSON.stringify(message));
+		}
+		catch (e) { 
+			deleteplayer(y);
+		}
+	}	
+}
+function deleteplayer(o){
+	Joueurs[o].ws.close();
+	console.log("Joueur %s deconnecté, %s joueurs restant", o , Joueurs.length);
+	Joueurs.splice(o,1); 
+	console.log("Joueur %s deconnecté, %s joueurs restant", o , Joueurs.length);
+	for(var y=0;y<Joueurs.length;y++){
+		var message = { 
+				type : "delserpents",
+				Serpent : TabSerpent,
+				index : y
 		};
-		ws2.send(JSON.stringify(message2));
-		setInterval(onFrame, 10);
-		function onFrame() 
-		{
-			update();
+		try { 
+			Joueurs[y].ws.send(JSON.stringify(message));
+		}
+		catch (e) { 
+			deleteplayer(y);
 		}
 	}
 }
 function update(){
-	
-	//console.log('update serpent 1.1 %s %s', Joueurs[1].Serpent.Tableau[0].x, Joueurs[1].Serpent.Tableau[0].y);
-	//console.log('update serpent 2.1 %s %s', Joueurs[1].Serpent.Tableau[1].x, Joueurs[1].Serpent.Tableau[1].y);
-	//console.log('update serpent 1 taille %s ', Joueurs[0].Serpent.Tableau.length);
+
+
 	for (var i = taille-1 ; i > 0 ; i--) {
-		var ax= Joueurs[0].Serpent.Tableau[i-1].x;
-		var ay= Joueurs[0].Serpent.Tableau[i-1].y;
-		var bx= Joueurs[1].Serpent.Tableau[i-1].x;
-		var by= Joueurs[1].Serpent.Tableau[i-1].y;
-		//console.log('update serpent 2b  %s %s', bx, by);
-		Joueurs[0].Serpent.Tableau[i].x= ax;
-		Joueurs[0].Serpent.Tableau[i].y= ay;
-		Joueurs[1].Serpent.Tableau[i].x= bx;
-		Joueurs[1].Serpent.Tableau[i].y= by;
-		//console.log('update serpent 1.%s %s %s',i, Joueurs[0].Serpent.Tableau[i].x, Joueurs[0].Serpent.Tableau[i].y);
-		//console.log('update serpent 2.%s %s %s',i, Joueurs[1].Serpent.Tableau[i].x, Joueurs[1].Serpent.Tableau[i].y);
+		for(var y=0;y<Joueurs.length;y++){
+
+			var ax= Joueurs[y].Serpent[i-1].x;
+			var ay= Joueurs[y].Serpent[i-1].y;
+
+			Joueurs[y].Serpent[i].x= ax;
+			Joueurs[y].Serpent[i].y= ay;
+		}
 	}
-	//console.log('update serpent 1.2 %s %s', Joueurs[1].Serpent.Tableau[0].x, Joueurs[1].Serpent.Tableau[0].y);
-	//console.log('update serpent 2.2 %s %s', Joueurs[1].Serpent.Tableau[1].x, Joueurs[1].Serpent.Tableau[1].y);
-	//console.log('update serpent 2b  %s %s', bx, by);
-	Joueurs[0].Serpent.Tableau[0].x += nexttete1[0];
-	Joueurs[0].Serpent.Tableau[0].y += nexttete1[1];
-	Joueurs[1].Serpent.Tableau[0].x += nexttete2[0];
-	Joueurs[1].Serpent.Tableau[0].y += nexttete2[1];
-	//console.log('update serpent 2b  %s %s', bx, by);
-	//console.log('update serpent 1.3 %s %s', Joueurs[1].Serpent.Tableau[0].x, Joueurs[1].Serpent.Tableau[0].y);
-	//console.log('update serpent 2.3 %s %s', Joueurs[1].Serpent.Tableau[1].x, Joueurs[1].Serpent.Tableau[1].y);
-	
-	var message1 = { 
-			type : "majserpents",
-			Serpent1 : Joueurs[0].Serpent.Tableau,
-			Serpent2 :Joueurs[1].Serpent.Tableau
-	};
-	ws1.send(JSON.stringify(message1));
-	
-	var message2 = { 
-			type : "majserpents",
-			Serpent1 : Joueurs[1].Serpent.Tableau,
-			Serpent2 : Joueurs[0].Serpent.Tableau
-	};
-	ws2.send(JSON.stringify(message2));
-	
+
+	for(var e=0;e<Joueurs.length;e++){
+		Joueurs[e].Serpent[0].x += Joueurs[e].nexttete[0];
+		Joueurs[e].Serpent[0].y += Joueurs[e].nexttete[1];
+	}
+
+
+
+	var TabSerpent=[];
+	for(var r=0;r<Joueurs.length;r++){
+		TabSerpent.push(Joueurs[r].Serpent);
+	}
+
+	for(var t=0;t<Joueurs.length;t++){
+		var message = { 
+				type : "majserpents",
+				Serpent : TabSerpent,
+				index : t
+		};
+		try { 
+			Joueurs[t].ws.send(JSON.stringify(message));
+		}
+		catch (e) { 
+			deleteplayer(t);
+		}
+
+	}
 }
 
 
